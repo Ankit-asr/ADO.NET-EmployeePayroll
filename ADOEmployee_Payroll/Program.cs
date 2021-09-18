99ea8760456377b05e5f2d66776ccb5a804e6b3e
Ankit Singh Rathore <asinghrathore0@gmail.com>
2021-09-18 13:14:45 +0530

    [Ankit] Add: Welcome Message

---
+++ ADOEmployee_Payroll/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ADOEmployee_Payroll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To ADO.NET Employee Payroll Service");
+        }
+    }
+}
